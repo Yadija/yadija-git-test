b66b7da94d81b2d88551a1009cd4e445035d3730
Yadija <ayuliadi01@gmail.com>
2022-06-10 23:01:57 +0800

    Menambahkan isi file index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque, est veritatis velit magni consequuntur voluptatem.</p>
 </body>
 </html>
